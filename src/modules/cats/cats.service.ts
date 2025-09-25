e9654ad9899cc9447254fbb44785f8b2dcd00367
YayaWeslla <yws2@aluno.ifal.edu.br>
2025-09-25 08:01:19 -0300

    quarto commit

--- src/modules/cats/cats.service.ts
+++ src/modules/cats/cats.service.ts
@@ -1,4 +1,6 @@
-import { Injectable } from '@nestjs/comon';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import {  }
+import { CreateCatDto } from './dtos/create-cat.dto.ts';
 
 
 @Injectable()
@@ -7,6 +9,19 @@ class CatsService {
 
     findAll(): Cat[] {
         return this.cats;
+    }
+
+    create(cat: CreateCatDto) Cat {
+        //verificar se o gato existe
+        const existCat = this.cats.find((c) => c.nome === catch.nome);
+
+        if (existCat) {
+            throw new BadRequestException('Este gato já existe');
+        }
+
+        //insere o gato no array
+        this.cats.push(cat);
+        return cat;
     }
 }
 
